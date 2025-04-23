92f22800992300365a84ab661b3af56fa6c946c0
creations <creations@creations.works>
2025-04-22 20:21:44 -0400

    i never actually added the var

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,7 @@ main().catch((error: Error) => {
 	logger.error(["Error initializing the server:", error]);
 	process.exit(1);
 });
+
+if (process.env.IN_PTERODACTYL === "true") {
+	console.log("Server Started");
+}
